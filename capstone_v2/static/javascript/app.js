89f7c2abd1c10e2ca6fd3ec31621044d0a40a5fe
tamarajwood <tjwood270@gmail.com>
2022-07-23 22:26:41 -0700

    preview blog posts displayed on home page, basic styles created

---
+++ capstone_v2/static/javascript/app.js
@@ -0,0 +1,27 @@
+const vm = new Vue({
+    el: '#app',
+    delimiters: ['[[', ']]'],
+    data: {
+        csrfToken: "",
+        postsList: [],
+    },
+    methods: {
+        loadPosts: function() {
+            axios({
+                method: 'get',
+                url: '/api/v1/posts/'
+            }).then(response => this.postsList = response.data)
+        }
+    },
+    created: function() {
+        this.loadPosts()
+    },
+    // mounted: function() {
+    //     this.csrfToken = document.querySelector("input[name=csrfmiddlewaretoken]").value
+        // this is causing an errpr but page still renders properly
+        // do I not need a csrfToken or is this something I need to ask on how
+        // to put into my vue app properly?
+        // TypeError: Cannot read properties of null (reading 'value')
+        // STILL GETTING ERROR EVEN AFTER COMMENT OUT
+    // }
+})
