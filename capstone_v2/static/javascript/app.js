f18483fc9c0442766bf1b38d5ea7f989f43a8251
tamarajwood <tjwood270@gmail.com>
2022-07-26 11:19:39 -0700

    for gage to look at

--- capstone_v2/static/javascript/app.js
+++ capstone_v2/static/javascript/app.js
@@ -14,11 +14,32 @@ const vm = new Vue({
                 method: 'get',
                 url: '/api/v1/posts/'
             }).then(response => this.postsList = response.data)
-        }
+        },
+        loadSubjects: function() {
+            console.log('cat')
+            this.postsList.forEach((value,index) =>{ 
+                let subjectItem = value[0]
+                if (subjectItem == 'hiking') {
+                    this.hikeList.unshift(post)
+                }
+                else if (subjectItem == 'cycling') {
+                    this.bikeList.unshift(post)
+                }
+                else if (subjectItem == 'mushing') {
+                    this.mushingList.unshift(post)
+                }
+            console.log('dog')
+                })
+        }, 
     },
+       
+    
     created: function() {
-        this.loadPosts()
+        this.loadPosts(),
+        this.loadSubjects()
     },
+})
+
     // mounted: function() {
     //     this.csrfToken = document.querySelector("input[name=csrfmiddlewaretoken]").value
         // this is causing an errpr but page still renders properly
@@ -26,5 +47,3 @@ const vm = new Vue({
         // to put into my vue app properly?
         // TypeError: Cannot read properties of null (reading 'value')
         // STILL GETTING ERROR EVEN AFTER COMMENT OUT
-    // }
-})
